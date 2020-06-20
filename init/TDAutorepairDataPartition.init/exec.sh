12618717f9c3d5474bf2d333188d8fc622396af0
410 <58301450+410-dev@users.noreply.github.com>
2020-06-21 01:49:10 +0900

    Will add mount directory

--- init/TDAutorepairDataPartition.init/exec.sh
+++ init/TDAutorepairDataPartition.init/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATA_COMPATIBILITY="10"
+export DATA_COMPATIBILITY="11"
 if [[ "$(mplxr SYSTEM/DATASETUP_COMPLETE)" == "null" ]]; then
 	echo "[*] Start data partition repair!"
 	rm -rf "$DATA/config" 2>/dev/null
@@ -16,14 +16,9 @@ if [[ "$(mplxr SYSTEM/DATASETUP_COMPLETE)" == "null" ]]; then
 	mplxw SYSTEM/GRAPHITE/present 1 >/dev/null
 	mplxw SYSTEM/COMMON/DataCompatibility "$DATA_COMPATIBILITY" >/dev/null
 elif [[ "$(mplxr SYSTEM/COMMON/DataCompatibility)" -ne "$DATA_COMPATIBILITY" ]]; then
-	echo "[*] Data partition must be restructured."
-	mv "$DATA/config" "$DATA/config.old"
-	mkdir -p "$DATA/init"
-	cp -r "$SYSTEM/TouchDown/multiplex" "$DATA/"
-	mplxw SYSTEM/machine_name "$(<$DATA/config.old/SYSTEM/machine_name)" >/dev/null
-	mplxw USER/user_name "$(<$DATA/config.old/USER/user_name)" >/dev/null
-	mplxw USER/SECURITY/PASSCODE "$(<$DATA/config.old/USER/SECURITY/PASSCODE)" >/dev/null
-	mplxw USER/SECURITY/PASSCODE_PRESENT "$(<$DATA/config.old/USER/SECURITY/PASSCODE_PRESENT)" >/dev/null
+	echo "[*] Upgrading data partition."
+	cp -r "$DATA/config" "$DATA/config.old"
+	mkdir -p "$DATA/mount"
 else
 	echo "[*] No task for data partition modification."
 fi

0ae25710ac9cfe5bd60e21daa83292905c4a46f7
410 <58301450+410-dev@users.noreply.github.com>
2020-07-01 22:49:57 +0900

    Updated binary detail

--- init/TDFilesystemSandbox.init/exec.sh
+++ init/TDFilesystemSandbox.init/exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "[*] Limiting RW permission..."
-echo "$(<"$1/rw")" > "$2/rwlim"
-echo "[*] Limiting Read permission..."
+echo "[*] Limiting write permission..."
+echo "$(<"$1/write")" > "$2/wlim"
+echo "[*] Limiting read permission..."
 echo "$(<"$1/read")" > "$2/rlim"
 echo "[*] Limiting catalog permission..."
 echo "$(<"$1/catalog")" > "$2/clim"

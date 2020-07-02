d4f40c9246a98553364785feec58a65627a9d4d9
410 <58301450+410-dev@users.noreply.github.com>
2020-07-02 17:32:39 +0900

    Framework now load under init

---
+++ init/Framework.init/exec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "[*] Running frameworks asyncronously..."
+mkdir -p "$CACHE/frameworks"
+Data="$DATA/logs"
+FrameworkLibrary="$SYSTEM/frameworks"
+cached="$CACHE/frameworks"
+mkdir -p "$Data"
+echo "[*] Reading frameworks loading priority..."
+exitCode=0
+logDate=$(date +"%Y-%m-%d-%H:%M")
+while [[ true ]]; do
+	if [[ -f "$FrameworkLibrary/LoadOrder" ]]; then
+		cat "$FrameworkLibrary/LoadOrder" | while read line
+		do
+			cd "$FrameworkLibrary"
+			SelectedFramework="$line"
+			if [[ -d "$line" ]]; then
+				ID=$(<"$SelectedFramework"/identifier)
+				echo "[*] Loading $ID..."
+				mkdir -p "$cached/$ID"
+				cd /Volumes/VFS
+				echo "LOAD: $ID" >> "$Data/output-frameworkloader-$logDate"
+				"$FrameworkLibrary/$SelectedFramework"/exec "$FrameworkLibrary/$SelectedFramework" "$cached/$ID" & >> "$Data/output-frameworkloader-$logDate" >/dev/null
+				ec=$?
+				if [[ $ec == 0 ]]; then
+					echo "[*] Load complete."
+				else
+					echo "[!] An error occured while loading $ID."
+					touch "$CACHE/load-failed"
+					exit
+				fi
+			else
+				echo "[!] Not existing framwork: $line"
+				echo "[!] Skipped."
+			fi
+		done
+		break
+	else
+		echo "[!] Frameworks priority not found at: $FrameworkLibrary/LoadOrder"
+		sleep 5
+	fi
+done
+exit $exitCode

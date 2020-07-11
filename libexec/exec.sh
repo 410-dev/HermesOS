ecf68aea484890875e838e629c61616cffbae003
410 <58301450+410-dev@users.noreply.github.com>
2020-07-11 18:52:01 +0900

    Need program start point to run

--- libexec/exec.sh
+++ libexec/exec.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
 if [[ -f "$USERDATA/$1" ]]; then
 	if [[ ! -z "$(echo "$b_args" | grep "verbose")" ]]; then
 		echo "[*] Verifying..."
+	fi
+	if [[ -z "$(cat "$USERDATA/$1" | grep "@PROG_START_POINT")" ]]; then
+		echo "Unable to start program: Unable to find @PROG_START_POINT"
+		exit 9
 	fi
 	cat "$USERDATA/$1" | while read fileLine
 	do

20ff8114249fd76747dd8fa79cab964d858566c7
410 <58301450+410-dev@users.noreply.github.com>
2020-07-14 10:52:28 +0900

    Now creates basic structure of /Data

--- init/TDBuildUserspace.init/exec.sh
+++ init/TDBuildUserspace.init/exec.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 mkdir -p "$USERDATA"
 mkdir -p "$DATA/Library/Developer"
+mkdir -p "$DATA/init"
+mkdir -p "$DATA/nvram"
+mkdir -p "$DATA/mount"
+mkdir -p "$DATA/config"
+mkdir -p "$DATA/init"

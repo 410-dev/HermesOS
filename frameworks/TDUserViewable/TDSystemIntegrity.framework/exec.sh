0730faa5c2a98180188f5003ca285a53ea3de7f2
410 <58301450+410-dev@users.noreply.github.com>
2020-07-14 10:51:27 +0900

    Longer wait time

--- frameworks/TDUserViewable/TDSystemIntegrity.framework/exec.sh
+++ frameworks/TDUserViewable/TDSystemIntegrity.framework/exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$(mplxr "SYSTEM/POLICY/isUsingImg")" == "TRUE" ]]; then
 	while [[ true ]]; do	
-		sleep 5
+		sleep 10
 		touch "$SYSTEM/integrity_test" 2>/dev/null
 		if [[ -f "$SYSTEM/integrity_test" ]]; then
 			echo "SYSTEM WARNING: System partition is writable!"

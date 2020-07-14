76a4684560c6c10384893f9d708dc916a20bcf64
410 <58301450+410-dev@users.noreply.github.com>
2020-07-14 12:29:57 +0900

    Removed deprecated codes

--- TouchDown/Deprecated/libexec/systeminfo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-profloc="$CACHE/init/TouchDown.System.SystemProfiler"
-echo "System Information"
-echo "OS Name: $(<$profloc/sys_name)"
-echo "Version: $(<$profloc/sys_version) (build $(<$profloc/sys_build))"
-echo "Compatibility: $(<$profloc/sys_compatibility)"
-echo "Manufacturer: $(<$profloc/sys_manufacture)"
-echo "Interface Version: $(<$profloc/interface_version)"
-exit 0
--- TouchDown/Deprecated/libexec/uirestart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Writing signal..."
-touch "$CACHE/SIG/shell_reload"

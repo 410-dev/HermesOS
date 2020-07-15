4e20d828a9a3d590a83868e08554b8f85cfaadb7
410 <58301450+410-dev@users.noreply.github.com>
2020-07-15 09:07:25 +0900

    Avoid duplication of hello message

--- init/TDSayHello.init/exec.sh
+++ init/TDSayHello.init/exec.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "TouchDownOS User Executive" >> "$CACHE/alert"
+if [[ -f "$CACHE/alert" ]]; then
+	if [[ -z "$(cat "$CACHE/alert" | grep "TouchDownOS User Executive")" ]]; then
+		echo "TouchDownOS User Executive" >> "$CACHE/alert"
+	fi
+else
+	echo "TouchDownOS User Executive" >> "$CACHE/alert"
+fi
+exit 0

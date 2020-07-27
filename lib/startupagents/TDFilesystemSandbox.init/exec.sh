01b790daebaea71ff7bfeddae1b9a46d99ceed10
410 <58301450+410-dev@users.noreply.github.com>
2020-07-27 09:24:50 +0900

    Changed startupagents

--- lib/startupagents/TDFilesystemSandbox.init/exec.sh
+++ lib/startupagents/TDFilesystemSandbox.init/exec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir -p "$CACHE/proc.faccess"
 echo "[*] Limiting write permission..."
-echo "$(<"$(dirname "$0")/write")" > "$CACHE/wlim"
+echo "$(<"$(dirname "$0")/write")" > "$CACHE/proc.faccess/wlim"
 echo "[*] Limiting read permission..."
-echo "$(<"$(dirname "$0")/read")" > "$CACHE/rlim"
+echo "$(<"$(dirname "$0")/read")" > "$CACHE/proc.faccess/rlim"
 echo "[*] Limiting catalog permission..."
-echo "$(<"$(dirname "$0")/catalog")" > "$CACHE/clim"
+echo "$(<"$(dirname "$0")/catalog")" > "$CACHE/proc.faccess/clim"
 exit 0

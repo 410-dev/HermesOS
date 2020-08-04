08f39f8ce014964b2c48efb9db9eefa0c852638c
410 <58301450+410-dev@users.noreply.github.com>
2020-08-04 09:26:09 +0900

    Bug fix

--- lib/hermes/Utility/Login.sh
+++ lib/hermes/Utility/Login.sh
@@ -4,13 +4,13 @@ if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "nothing" ]] || [[ "$(mplxr USER/SECU
 else
 	if [[ "$(mplxr SYSTEM/GRAPHITE/ENFORCE_CLI)" == "0" ]] && [[ $(bootarg.contains "enforce_cli") == 0 ]]; then
 		while [[ true ]]; do
-			export PASS=$("$libcarbondraw/TDGraphicalUIRenderer" --stdout --passwordbox "Please enter your password." 0 0)
+			export PASS=$("$libcarbondraw/engine" --stdout --passwordbox "Please enter your password." 0 0)
 			if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "$(md5 -qs $PASS)" ]]; then
 				verbose "[*] Login successful."
 				clear
 				break
 			else
-				"graphite_msgbox" "Login Failed" "Wrong password."
+				"carbondraw_msgbox" "Login Failed" "Wrong password."
 			fi
 		done
 	else

1b7abb4d386501f989a631ae757fb030ac475a6d
410 <58301450+410-dev@users.noreply.github.com>
2020-09-18 01:51:39 +0900

    Update multiplex data

--- lib/hermes/Utility/convert.sh
+++ lib/hermes/Utility/convert.sh
@@ -2,4 +2,5 @@
 export PREV_MAJOR="$1"
 export PREV_MINOR="$2"
 export PREV_EDIT="$3"
-verbose "[*] No registered action."
+verbose "[*] Updating multiplex data..."
+cp -nr "$SYSTEMSUPPORT/defaults/multiplex/"* "$LIBRARY/config/"

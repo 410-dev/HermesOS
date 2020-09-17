91fc3d401744a07efc201584478a9748a341dc4a
410 <58301450+410-dev@users.noreply.github.com>
2020-09-18 00:01:36 +0900

    Authenticate user password

---
+++ lib/hermes/Utility/Authenticate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Process \"$1\" is trying to access $2. Would you allow this? (Y/n)"
+read in
+if [[ "$in" == "y" ]] || [[ "$in" == "Y" ]]; then
+	if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "nothing" ]] || [[ "$(mplxr USER/SECURITY/PASSCODE_PRESENT)" == "0" ]]; then
+		exit 0
+	else
+		for (( i = 0; i < 3; i++ )); do
+			echo -n "Please enter your password: "
+			read -s PASS
+			if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "$(md5 -qs $PASS)" ]]; then
+				exit 0
+			else
+				echo "Error: Password does not match."
+			fi
+		done
+		echo "Sorry, you have entered wrong password too many times."
+		exit 1
+	fi
+elif [[ "$in" == "n" ]] || [[ "$in" == "N" ]]; then
+	exit 1
+else
+	echo "Unknown input. Interpreted as decline."
+	exit 1
+fi
+
+

7560176fa0b2a96ab457d817293eb21646641f3a
410 <58301450+410-dev@users.noreply.github.com>
2020-09-17 23:53:42 +0900

    Limited login attempt to 5 times

--- lib/hermes/Utility/Login.sh
+++ lib/hermes/Utility/Login.sh
@@ -3,26 +3,36 @@ if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "nothing" ]] || [[ "$(mplxr USER/SECU
 	verbose "[*] Login successful (weak)."
 else
 	if [[ "$(mplxr SYSTEM/GRAPHITE/ENFORCE_CLI)" == "0" ]] && [[ $(bootarg.contains "enforce_cli") == 0 ]]; then
-		while [[ true ]]; do
+		export successful=0
+		for (( i = 0; i < 5; i++ )); do
 			export PASS=$("$libcarbondraw/engine" --stdout --passwordbox "Please enter your password." 0 0)
 			if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "$(md5 -qs $PASS)" ]]; then
 				verbose "[*] Login successful."
+				export successful=1
 				clear
 				break
 			else
 				"carbondraw_msgbox" "Login Failed" "Wrong password."
 			fi
 		done
+		if [[ "$successful" == "0" ]]; then
+			"$SYSTEMSUPPORT/Utility/Lock" "TooManyLoginAttempt"
+		fi
 	else
-		while [[ true ]]; do
+		export successful=0
+		for (( i = 0; i < 5; i++ )); do
 			echo -n "Please enter your password: "
 			read -s PASS
 			if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "$(md5 -qs $PASS)" ]]; then
 				echo "[*] Login successful."
+				export successful=1
 				break
 			else
 				echo "[-] Login failed."
 			fi
 		done
+		if [[ "$successful" == "0" ]]; then
+			"$SYSTEMSUPPORT/Utility/Lock" "TooManyLoginAttempt"
+		fi
 	fi
 fi

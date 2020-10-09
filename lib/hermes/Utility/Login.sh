c1b2a63b09c9cce01d6f6b8825652ffa9ee3dffe
410 <58301450+410-dev@users.noreply.github.com>
2020-10-09 10:10:32 +0900

    Shows username on login

--- lib/hermes/Utility/Login.sh
+++ lib/hermes/Utility/Login.sh
@@ -2,6 +2,7 @@
 if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "nothing" ]] || [[ "$(mplxr USER/SECURITY/PASSCODE_PRESENT)" == "0" ]]; then
 	verbose "[*] Login successful (weak)."
 else
+	echo "User: $(mplxr "USER/user_name")"
 	export successful=0
 	for (( i = 0; i < 5; i++ )); do
 		echo -n "Please enter your password: "

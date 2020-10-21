05cf8cb22040762331f73de57eaac004260efdcd
410 <58301450+410-dev@users.noreply.github.com>
2020-10-21 15:07:09 +0900

    OTA Download utility

---
+++ sys/Library/Services/Update/dlutil.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ -f "$LIBRARY/image.zip" ]]; then
+	echo "You already have an image downloaded."
+	echo -n "Do you want to continue? (Y/n): "
+	read yn
+	if [[ "$yn" == "Y" ]] || [[ "$yn" == "y" ]]; then
+		echo -n ""
+	else
+		echo "Aborted."
+		exit 0
+	fi
+fi
+echo "Checking tag..."
+export URL="$(<"$(dirname "$0")/ota-profile")"
+if [[ -f "$NVRAM/ota-profile" ]]; then
+	export URL="$(<"$NVRAM/ota-profile")"
+fi
+curl -Ls "$URL" -o "$CACHE/ota-profile"
+source "$CACHE/ota-profile"
+if [[ ! -z "$(<"$(dirname "$0")/ota-address")" ]] && [[ ! -z "$(<"$(dirname "$0")/ota-address")" ]]; then
+	curl -L --progress-bar "$(<"$(dirname "$0")/ota-address")/$OS_Tag/$(<"$(dirname "$0")/ota-filename")" -o "$LIBRARY/image.zip"
+	if [[ ! -z "$(cat "$LIBRARY/image.zip" | grep "lib/hermes")" ]]; then
+		echo "Download was successful: $OS_Tag"
+		exit 0
+	else
+		echo "Failed downloading: $OS_Tag"
+		rm "$LIBRARY/image.zip"
+		exit 0
+	fi
+else
+	echo "Internal Error: Address not found."
+	exit 0
+fi

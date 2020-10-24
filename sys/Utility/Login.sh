8a14f74c005e2e805c430617e2218520fa308177
410 <58301450+410-dev@users.noreply.github.com>
2020-10-24 15:04:10 +0900

    Bug fix

--- sys/Utility/Login.sh
+++ sys/Utility/Login.sh
@@ -5,15 +5,16 @@ if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "nothing" ]] || [[ "$(mplxr USER/SECU
 else
 	export successful=0
 	for (( i = 0; i < 5; i++ )); do
-		export PASS="$("$OSSERVICES/hermes/Library/display" --stdout --passwordbox "$2 to: $(mplxr "USER/user_name")" 0 0)"
+		export PASS="$("$OSSERVICES/Library/display" --stdout --passwordbox "$2 to: $(mplxr "USER/user_name")" 0 0)"
 		echo ""
 		if [[ "$(mplxr USER/SECURITY/PASSCODE)" == "$(md5 -qs $PASS)" ]]; then
 			echo -e "$2 successful."
 			export successful=1
+			clear
 			exit 0
 		else
 			echo -e "$2 failed."
-			"$OSSERVICES/hermes/Library/display" --infobox "$2 failed." 0 0
+			"$OSSERVICES/Library/display" --infobox "$2 failed." 0 0
 			sleep 1
 		fi
 	done

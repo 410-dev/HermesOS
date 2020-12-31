25f8693d4df3a6ce69c379151fb74ff225009a2e
410 <58301450+410-dev@users.noreply.github.com>
2020-12-31 16:01:42 +0900

    Bug fix

--- bin/super.sh
+++ bin/super.sh
@@ -4,6 +4,10 @@
 if [[ "$?" == 0 ]]; then
 	declare -i HUID
 	export HUID="0"
-	"$SYSTEM/bin/$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+	if [[ -f "$SYSTEM/bin/$1" ]]; then
+		"$SYSTEM/bin/$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+	else
+		echo "super: $1: command not found."
+	fi
 fi
 export HUID="1"

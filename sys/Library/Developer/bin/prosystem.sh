614fecade8498239fc2e22203adeed383b71d4d6
410 <58301450+410-dev@users.noreply.github.com>
2021-01-04 16:46:50 +0900

    Automatically lock / unlock

--- sys/Library/Developer/bin/prosystem.sh
+++ sys/Library/Developer/bin/prosystem.sh
@@ -2,8 +2,10 @@
 if [[ "$1" == "--set-key" ]]; then
 	if [[ "$2" == "1" ]]; then
 		echo "Pro System" > "$NVRAM/security/prosys"
+		echo "Unlocked" > "$NVRAM/security/lockstate"
 	else
 		echo "Default" > "$NVRAM/security/prosys"
+		echo "Locked" > "$NVRAM/security/lockstate"
 	fi
 else
 	echo "Unknown Action."

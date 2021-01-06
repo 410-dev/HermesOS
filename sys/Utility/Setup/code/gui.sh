e3aae9a66397eb261abcd23cff0737d750e72fb4
410 <58301450+410-dev@users.noreply.github.com>
2021-01-06 11:56:20 +0900

    More setup content

--- sys/Utility/Setup/code/gui.sh
+++ sys/Utility/Setup/code/gui.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 verbose "[${GREEN}*${C_DEFAULT}] Starting graphical setup..."
+
+source "$BundlePath/code/languages_selectable"
+export language="$("$SYSTEM/sys/Library/display" --stdout --radiolist "Select Language" 0 0 0 $langd)"
+if [[ -z "$language" ]]; then
+	echo "You have selected: ${LANG_ID}"
+elif [[ -f "$SYSTEM/sys/Default/Languages/$language" ]]; then
+	echo "${SETTING_LANGUAGE}"
+	rm -rf "$LIBRARY/Preferences/Language/system.hlang"
+	cp "$SYSTEM/sys/Default/Languages/$language" "$LIBRARY/Preferences/Language"
+	mv "$LIBRARY/Preferences/Language/$language" "$LIBRARY/Preferences/Language/system.hlang"
+	source "$LIBRARY/Preferences/Language/system.hlang"
+	echo "${LANGUAGE_CHANGED_ON_SETUP}"
+else
+	echo "No such language."
+fi
+
 while [[ true ]]; do
 	export USRNAME="$("$SYSTEM/sys/Library/display" --stdout --inputbox "What is your name?" 0 0)"
 	if [[ -z "$USRNAME" ]]; then
@@ -40,5 +56,37 @@ while [[ "$PASSPRESENT" == "1" ]]; do
 		fi
 	fi
 done
+"$SYSTEM/sys/Library/display" --title "Setup" --yesno "Do you want to enable Pro System? (System protection will be disabled)" 0 0
+export PROSYS=$?
+if [[ "$PROSYS" == "0" ]]; then
+	export PROSYS="1"
+else
+	export PROSYS="0"
+fi
+if [[ "$PROSYS" == "1" ]]; then
+	mkdir -p "$NVRAM/security"
+	echo "Pro System" > "$NVRAM/security/prosys"
+	echo "Unlocked" > "$NVRAM/security/lockstate"
+elif [[ "$PROSYS" == "0" ]]; then
+	mkdir -p "$NVRAM/security"
+	echo "Default" > "$NVRAM/security/prosys"
+	echo "Locked" > "$NVRAM/security/lockstate"
+fi
+if [[ "$PROSYS" == 1 ]] && [[ "$PASSPRESENT" == "1" ]]; then
+	"$SYSTEM/sys/Library/display" --title "Setup" --yesno "Do you want to enable auto-login?" 0 0
+	export AUTOLOGIN=$?
+	if [[ "$AUTOLOGIN" == "0" ]]; then
+		export AUTOLOGIN="1"
+	else
+		export AUTOLOGIN="0"
+	fi
+	if [[ "$AUTOLOGIN" == "1" ]]; then
+		mkdir -p "$NVRAM/security"
+		echo "system" > "$NVRAM/security/autologin"
+	elif [[ "$AUTOLOGIN" == "0" ]]; then
+		mkdir -p "$NVRAM/security"
+		echo "" > "$NVRAM/security/autologin"
+	fi
+fi
 "$BundlePath/code/writer"
 clear
--- sys/Utility/Setup/code/nogui.sh
+++ sys/Utility/Setup/code/nogui.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 verbose "[${GREEN}*${C_DEFAULT}] Starting non-graphical setup..."
+ls -1 "$SYSTEM/sys/Default/Languages"
+while [[ true ]]; do
+	echo ""
+	echo "To keep your current language, just press enter."
+	echo -n "Choose your language: "
+	read language
+	if [[ -z "$language" ]]; then
+		echo "You have selected: ${LANG_ID}"
+	elif [[ -f "$SYSTEM/sys/Default/Languages/$language" ]]; then
+		echo "${SETTING_LANGUAGE}"
+		rm -rf "$LIBRARY/Preferences/Language/system.hlang"
+		cp "$SYSTEM/sys/Default/Languages/$language" "$LIBRARY/Preferences/Language"
+		mv "$LIBRARY/Preferences/Language/$language" "$LIBRARY/Preferences/Language/system.hlang"
+		source "$LIBRARY/Preferences/Language/system.hlang"
+		echo "${LANGUAGE_CHANGED_ON_SETUP}"
+	else
+		echo "No such language."
+	fi
+done
 while [[ true ]]; do
 	echo -n "What is your name? (English and Number only, no spacebar) : "
 	read USRNAME
 	if [[ -z "$USRNAME" ]]; then
-		echo "[-] Invalid input."
+		echo "[-] ${INVALID_INPUT}"
 	else
 		mplxw "USER/user_name" "$USRNAME" >/dev/null
 		break
@@ -14,7 +33,7 @@ while [[ true ]]; do
 	echo -n "What is your device name? (English and Number only, no spacebar) : "
 	read DEVN
 	if [[ -z "$DEVN" ]]; then
-		echo "[-] Invalid input."
+		echo "[-] ${INVALID_INPUT}"
 	else
 		mplxw "SYSTEM/machine_name" "$DEVN" >/dev/null
 		break
@@ -34,7 +53,7 @@ while [[ true ]]; do
 				read -s PASS
 				echo ""
 				if [[ -z "$PASS" ]]; then
-					echo "[-] Invalid input."
+					echo "[-] ${INVALID_INPUT}"
 				else
 					mplxw "USER/SECURITY/PASSCODE" "$(md5 -qs $PASS)" >/dev/null
 					break
@@ -43,7 +62,43 @@ while [[ true ]]; do
 		done
 		break
 	else
-		echo "[-] Invalid input."
+		echo "[-] ${INVALID_INPUT}"
 	fi
 done
+while [[ true ]]; do
+	echo -n "Would you enable Pro System? (System protection will be disabled) yes / no: "
+	read yn
+	if [[ -z "$yn" ]]; then
+		echo "[-] ${INVALID_INPUT}"
+	elif [[ "$yn" == "YES" ]] || [[ "$yn" == "Yes" ]] || [[ "$yn" == "yes" ]]; then
+		mkdir -p "$NVRAM/security"
+		echo "Pro System" > "$NVRAM/security/prosys"
+		echo "Unlocked" > "$NVRAM/security/lockstate"
+		break
+	elif [[ "$yn" == "NO" ]] || [[ "$yn" == "No" ]] || [[ "$yn" == "no" ]]; then
+		mkdir -p "$NVRAM/security"
+		echo "Default" > "$NVRAM/security/prosys"
+		echo "Locked" > "$NVRAM/security/lockstate"
+		break
+	else
+		echo "[-] ${INVALID_INPUT}"
+	fi
+done
+if [[ "$PROSYS" == 1 ]] && [[ "$PASSPRESENT" == "1" ]]; then
+	while [[ true ]]; do
+		echo -n "Do you want to enable auto-login? yes / no"
+		read AUTOLOGIN
+		if [[ "$AUTOLOGIN" == "YES" ]] || [[ "$AUTOLOGIN" == "Yes" ]] || [[ "$AUTOLOGIN" == "yes" ]]; then
+			mkdir -p "$NVRAM/security"
+			echo "system" > "$NVRAM/security/autologin"
+			break
+		elif [[ "$AUTOLOGIN" == "NO" ]] || [[ "$AUTOLOGIN" == "No" ]] || [[ "$AUTOLOGIN" == "no" ]]; then
+			mkdir -p "$NVRAM/security"
+			echo "" > "$NVRAM/security/autologin"
+			break
+		else
+			echo "[-] ${INVALID_INPUT}"
+		fi
+	done
+fi
 "$BundlePath/code/writer"

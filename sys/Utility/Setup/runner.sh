f5ecff43bf6e51cb6797e58e6528a3d2de58200a
410 <58301450+410-dev@users.noreply.github.com>
2021-01-06 11:43:54 +0900

    GUI enabled

--- sys/Utility/Setup/runner.sh
+++ sys/Utility/Setup/runner.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-"$BundlePath/code/nogui"
-
-# if [[ $(bootArgumentHas "verbose") == "1" ]] || [[ $(bootArgumentHas "nogui") == "1" ]] || [[ $(bootArgumentHas "safe") == "1" ]]; then
-# 	"$BundlePath/code/nogui"
-# else
-# 	"$BundlePath/code/gui"
-# fi
+if [[ $(bootArgumentHas "verbose") == "1" ]] || [[ $(bootArgumentHas "nogui") == "1" ]] || [[ $(bootArgumentHas "safe") == "1" ]]; then
+	"$BundlePath/code/nogui"
+else
+	"$BundlePath/code/gui"
+fi

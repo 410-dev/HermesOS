d2a67aeada4f3b66a9bd777b98f6dd677dd69d35
410 <58301450+410-dev@users.noreply.github.com>
2021-01-22 10:51:54 +0900

    Added log (2) + optimising

--- sys/Utility/preload.sh
+++ sys/Utility/preload.sh
@@ -4,9 +4,11 @@ echo "HermesOS $OS_Version"
 if [[ ! -f "$OSSERVICES/Utility/$(mplxr USER/INTERFACE/START_MODE)/runner" ]]; then
 	echo "⛔️"
 	verbose "Unable to load preload utility."
+	sys_log "preload" "Preload specification not found!"
 	while [[ true ]]; do
 		sleep 1000
 	done
 else
+	sys_log "preload" "Starting preload: $(mplxr USER/INTERFACE/START_MODE)"
 	aopen "$OSSERVICES/Utility/$(mplxr USER/INTERFACE/START_MODE)"
 fi
--- sys/interface.sh
+++ sys/interface.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
+clear_setuplang
+sys_log "interface" "Cleared setup language set."
 function loadDefinition() {
 	cd "$CORE/extensions"
 	while read defname
 	do
+		sys_log "interface" "Reloading memory link: $defname"
 		verbose "${REFRESHING_MEMORYLINK}$defname"
 		source "$CORE/extensions/$defname"
+		sys_log "interface" "Memory link updated: $defname"
 	done <<< "$(ls -p | grep -v / | grep ".hmref")"
 }
 
-if [[ "$(mplxr USER/SECURITY/LOGIN_ATTEMPT)" == "64" ]]; then
-	echo -e "${RED}${ERROR}${UI_START_FAIL}"
-	echo -e "${RED}${ERROR_CODE}64"
-	echo -e "${RED}${PASS_TOOMANY_ATTEMPTS}${C_DEFAULT}"
-	exit 0
-fi
+sys_log "interface" "Running preload..."
 "$OSSERVICES/Utility/preload"
-if [[ "$(mplxr USER/SECURITY/LOGIN_ATTEMPT)" == "64" ]]; then
-	echo -e "${RED}${ERROR}${UI_START_FAIL}"
-	echo -e "${RED}${ERROR_CODE}64"
-	echo -e "${RED}${PASS_TOOMANY_ATTEMPTS}${C_DEFAULT}"
-	exit 0
-fi
-
+sys_log "interface" "Logged in."
+sys_log "interface" "Getting registry data..."
 export USERN=$(mplxr "USER/user_name")
 export MACHN=$(mplxr "SYSTEM/machine_name")
 if [[ -z "$USERN" ]]; then
@@ -33,30 +27,29 @@ fi
 cd "$ROOTFS"
 
 while [[ true ]]; do
-
+	sys_log "interface" "Setting default permission value..."
 	declare -i HUID
 	if [[ "$OS_UnlockedDistro" == "Unlocked" ]]; then
 		export HUID="0"
+		sys_log "interface" "System is unlocked. Setting permission value to 0."
 	else
 		export HUID="1"
+		sys_log "interface" "System is locked. Setting permission value to 1."
 	fi
 
 	if [[ -f "$CACHE/alert" ]] ; then
+		sys_log "interface" "Alert present: $(cat "$CACHE/alert")"
 		cat "$CACHE/alert"
 		rm "$CACHE/alert"
 	fi
-	if [[ "$(mplxr USER/INTERFACE/ALERT_PRESENT)" == "1" ]] ; then
-		mplxr "USER/INTERFACE/ALERT"
-		mplxw "USER/INTERFACE/ALERT_PRESENT" "0"
-		mplxw "USER/INTERFACE/ALERT" ""
-	fi
 	echo -en "${GREEN}${USERN}${C_DEFAULT}@${BLUE}${MACHN}${C_DEFAULT} ~ # "
 	read command
+	sys_log "interface" "Recieved input: $command"
 	export args=($command)
-	
 	echo "${args[0]}" > "$CACHE/process"
 	if [[ "${args[0]}" == "../"* ]]; then
 		echo -e "$ESCAPE_NOT_ALLOWED"
+		sys_log "interface" "Escape detected."
 	elif [[ -f "$SYSTEM/bin/${args[0]}" ]]; then
 		if [[ "${args[0]}" == "lec" ]]; then
 			echo -n ""
@@ -83,17 +76,23 @@ while [[ true ]]; do
 		echo "${COMMAND_NOT_FOUND}${args[0]}"
 	fi
 	if [[ -f "$CACHE/stdown" ]]; then
+		sys_log "interface" "Shutdown signal recieved."
+		sys_log "interface" "Flushing cache data..."
 		verbose "$FLUSHING_CACHE_DATA"
 		rm -rf "$CACHE"
 		exit 0
 	elif [[ -f "$CACHE/rboot" ]]; then
+		sys_log "interface" "Reboot signal recieved."
+		sys_log "interface" "Flushing cache data..."
 		verbose "$FLUSHING_CACHE_DATA"
 		rm -rf "$CACHE"
 		exit 100
 	elif [[ -f "$CACHE/uirestart" ]]; then
+		sys_log "interface" "Restarting interface!"
 		rm -f "$CACHE/uirestart"
 		exit 101
 	elif [[ -f "$CACHE/defreload" ]]; then
+		sys_log "interface" "Reloading memory links..."
 		echo "$RELOAD_WARNING"
 		loadDefinition
 	fi

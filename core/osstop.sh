a1cdb06e0b8ec27ae586c8e8ccc2ad7cf5bd8538
410 <58301450+410-dev@users.noreply.github.com>
2021-01-22 18:50:12 +0900

    Added logs to extensions

--- core/osstop.sh
+++ core/osstop.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 verbose "[${GREEN}*${C_DEFAULT}] Loading lists of backgroundworkers..."
+sys_log "OSStop" "Listing backgroundworkers..."
 ALIVE=$(ps -ax | grep "$CORE/extensions[/]")
+sys_log "OSStop" "Background workers: $ALIVE"
 verbose "[${GREEN}*${C_DEFAULT}] Killing syncronously..."
+sys_log "OSStop" "Killing all threads syncronously..."
 verbose "$ALIVE" | while read proc
 do
 	if [[ ! -z "$proc" ]]; then
 		frpid=($proc)
 		kill -9 ${frpid[0]}
+		sys_log "OSStop" "Killed: ${frpid[0]}"
 		verbose "[${GREEN}*${C_DEFAULT}] Killed PID: ${frpid[0]}"
 	fi
 done
+sys_log "OSStop" "All workers closed."
 verbose "[${GREEN}*${C_DEFAULT}] Background workers are closed."
--- core/uistart.sh
+++ core/uistart.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 if [[ -f "$OSSERVICES/interface" ]]; then
+	sys_log "UIStart" "OS Interface found."
 	export exitcode=1
 	while [[ $exitcode -ne 0 ]] && [[ $exitcode -ne 100 ]]; do
+		sys_log "UIStart" "Instruction: $exitcode"
+		sys_log "UIStart" "Launching interface!"
 		"$OSSERVICES/interface"
 		exitcode="$?"
 	done
+	sys_log "UIStart" "Instruction: $exitcode"
+	sys_log "UIStart" "Stopping OS..."
 	"$CORE/osstop"
 	exit "$exitcode"
 else
+	sys_log "UIStart" "OS Interface not found."
+	sys_log "UIStart" "Stopping core."
 	echo -e "[${RED}-${C_DEFAULT}] OS Interface not found."
 	echo -e "[${RED}-${C_DEFAULT}] Stopping core."
 	"$CORE/osstop"

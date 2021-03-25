cd878fd4084a1461a70cdb8beda4c95ed12d8841
410 <58301450+410-dev@users.noreply.github.com>
2021-03-25 10:00:08 +0900

    exec command allows run external isa if pro mode

--- bin/exec.sh
+++ bin/exec.sh
@@ -14,7 +14,7 @@ elif [[ -f "$USERDATA/$1" ]]; then
 	do
 		echo "$QuarantineData" | while read disabledCommand
 		do
-			if [[ $(echo "$fileLine") ==  "$disabledCommand "* ]]; then
+			if [[ $(echo "$fileLine") ==  "$disabledCommand "* ]] && [[ "$OS_ProSystemStatus" -ne "Pro System" ]]; then
 				echo "Execution disabled by sandbox."
 				cd "$DATA"
 				exit 9

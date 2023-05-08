3f8f1068a289fd805f79a5b11b4eaef1dd8151a0
410 <58301450+410-dev@users.noreply.github.com>
2023-05-08 03:04:24 -0400

    OSStart accepts more parameters

--- boot/init.sh
+++ boot/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+clear
 source "$(dirname "$0")/partitions.hdp"
 source "$(dirname "$0")/internal_func"
 source "$(dirname "$0")/bootloader/bootconf"
-OSSTART "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+OSSTART "$@"
 leaveSystem

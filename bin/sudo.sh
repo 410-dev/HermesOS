caede5f9e7f262fd105dd1333f202ff435fd8f19
410 <58301450+410-dev@users.noreply.github.com>
2023-07-10 19:12:23 -0400

    Sudo now allows developer options to run

--- bin/sudo.sh
+++ bin/sudo.sh
@@ -5,7 +5,17 @@ if [[ "$?" == 0 ]]; then
 	declare -i HUID
 	export HUID="0"
 	if [[ -f "$SYSTEM/bin/$1" ]]; then
-		"$SYSTEM/bin/$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+		COMMAND="$SYSTEM/bin/$1"
+		shift
+		"$COMMAND" "$@"
+	elif [[ "$(regread USER/Shell/DeveloperOptions)" == "1" ]]; then
+		if [[ -f "$OSSERVICES/Library/Developer/man/$1" ]]; then
+			COMMAND="$OSSERVICES/Library/Developer/bin/$1"
+			shift
+			"$COMMAND" "$@"
+		else
+			echo "sudo: $1: command not found."
+		fi
 	else
 		echo "sudo: $1: command not found."
 	fi

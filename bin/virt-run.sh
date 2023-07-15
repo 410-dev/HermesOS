d7da2733f0e12c65ede9b529b12df5ce61cec64f
410 <58301450+410-dev@users.noreply.github.com>
2023-07-14 23:23:24 -0400

    virt-run will detect legacy instance

--- bin/virt-run.sh
+++ bin/virt-run.sh
@@ -48,14 +48,66 @@ fi
 
 # Check legacy and patch boot files according to it.
 echo "Patching partition map..."
-rm "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partitions.hdp"
-if [[ -f "$USERDATA/VirtualMachines/configs/$ContainerName/legacy" ]]; then
-    echo "Patching for legacy mode (1x)..."
-    cp "$OSSERVICES/Library/Virtualization/Patches/Partitions/Hermes1x.hdp" "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partinf.hdp"
-else
-    echo "Patching for normal mode (2x)..."
-    cp "$OSSERVICES/Library/Virtualization/Patches/Partitions/Hermes2x.hdp" "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partinf.hdp"
+
+if [[ -f "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partitions.hdp" ]]; then
+    rm "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partitions.hdp"
 fi
+
+patch_os_mode() {
+    local mode=$1
+    if [[ "$mode" == "legacy" ]]; then
+        local mode_version="1x"
+    else
+        local mode_version="2x"
+    fi
+
+    echo "Patching for $mode mode ($mode_version)..."
+    cp "$OSSERVICES/Library/Virtualization/Patches/Partitions/Hermes${mode_version}.hdp" "$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partinf.hdp"
+
+    if [[ "$mode" == "legacy" ]]; then
+        echo "true" > "$USERDATA/VirtualMachines/configs/$ContainerName/legacy"
+    else
+        rm "$USERDATA/VirtualMachines/configs/$ContainerName/legacy"
+    fi
+}
+
+user_prompt() {
+    echo -e "${YELLOW}It seems that the OS is upgraded from $1 mode to $2 mode.${NC}"
+    echo -e "${YELLOW}If you want to use $1 mode, please press 1.${NC}"
+    echo -e "${YELLOW}If you want to use $2 mode, please press 2.${NC}"
+    read -r -n 1 -s
+
+    case "$REPLY" in
+        1)
+            patch_os_mode "$1"
+            ;;
+        2)
+            patch_os_mode "$2"
+            ;;
+        *)
+            echo "Invalid input."
+            exit 90
+            ;;
+    esac
+}
+
+legacy_flag="$USERDATA/VirtualMachines/configs/$ContainerName/legacy"
+common_compatibility="$USERDATA/VirtualMachines/containers/$ContainerName/disk/System/sys/Default/registry/SYSTEM/Common/CommonCompatibility"
+
+if [[ -f "$legacy_flag" ]]; then
+    if [[ -f "$common_compatibility" ]]; then
+        user_prompt "legacy" "normal"
+    else
+        patch_os_mode "legacy" "1x"
+    fi
+else
+    if [[ ! -f "$common_compatibility" ]]; then
+        user_prompt "normal" "legacy"
+    else
+        patch_os_mode "normal" "2x"
+    fi
+fi
+
 
 sed "s|CONTAINER_NAME|$ContainerName|g" $USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partinf.hdp > $USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partitions.hdp
 rm $USERDATA/VirtualMachines/containers/$ContainerName/disk/System/boot/partinf.hdp

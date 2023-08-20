cc8473a10c96a25c0b8e28a448f00ba688665bb9
410-dev <58301450+410-dev@users.noreply.github.com>
2023-08-21 00:55:38 +0900

    man and fasthelp will use path to probe manuals

--- bin/fasthelp.sh
+++ bin/fasthelp.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 echo "Installed binaries:"
 echo "___________________"
-ls -1 "$SYSTEM/man"
-if [[ "$(regread USER/Shell/DeveloperOptions)" == "1" ]]; then
-	ls -1 "$OSSERVICES/Library/Developer/man"
+# ls -1 "$SYSTEM/man"
+# if [[ "$(regread USER/Shell/DeveloperOptions)" == "1" ]]; then
+# 	ls -1 "$OSSERVICES/Library/Developer/man"
+# fi
+
+SEARCHPATHS2="$(regread "SYSTEM/Path")"
+if [[ ! -z "$SEARCHPATHS2" ]] && [[ "$SEARCHPATHS2" != "null" ]] && [[ "$SEARCHPATHS2" != "default" ]] && [[ "$SEARCHPATHS2" != "null" ]]; then
+	export SEARCHPATHS="$SEARCHPATHS2"
+else
+	regwrite "SYSTEM/Path" "$SEARCHPATHS" > /dev/null
 fi
+
+IFS=':' read -ra ListOfPath <<< "$SEARCHPATHS"
+FOUND=0
+for pth in "${ListOfPath[@]}"; do
+	pth="$ROOTFS$pth"
+	if [[ ! -d "$pth/man" ]]; then
+		continue
+	else
+		ls -1 "$pth/man"
+	fi
+done
+
 exit 0
--- bin/man.sh
+++ bin/man.sh
@@ -3,15 +3,29 @@ if [[ -z "$1" ]]; then
 	echo "${MISSING_PARAM}command"
 	exit 90
 fi
-if [[ -f "$SYSTEM/man/$1" ]]; then
-	cat "$SYSTEM/man/$1"
-elif [[ "$(regread USER/Shell/DeveloperOptions)" == "1" ]]; then
-	if [[ -f "$OSSERVICES/Library/Developer/man/$1" ]]; then
-		cat "$OSSERVICES/Library/Developer/man/$1"
-	else
-		echo "No manual available."
-	fi
+
+SEARCHPATHS2="$(regread "SYSTEM/Path")"
+if [[ ! -z "$SEARCHPATHS2" ]] && [[ "$SEARCHPATHS2" != "null" ]] && [[ "$SEARCHPATHS2" != "default" ]] && [[ "$SEARCHPATHS2" != "null" ]]; then
+	export SEARCHPATHS="$SEARCHPATHS2"
 else
-	echo "No manual available."
+	regwrite "SYSTEM/Path" "$SEARCHPATHS" > /dev/null
 fi
+
+IFS=':' read -ra ListOfPath <<< "$SEARCHPATHS"
+FOUND=0
+for pth in "${ListOfPath[@]}"; do
+	pth="$ROOTFS$pth"
+	if [[ ! -d "$pth/man" ]]; then
+		continue
+	fi
+	if [[ -f "$pth/man/$1" ]]; then
+		cat "$pth/man/$1"
+		FOUND=1
+	fi
+done
+
+if [[ "$FOUND" -eq "0" ]]; then
+	echo "No manual avaliable: $1"
+fi
+
 exit 0

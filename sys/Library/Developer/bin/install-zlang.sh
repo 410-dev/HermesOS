a1e2d2af0e5122b35163cb16ff6f8c32a670c25b
410-dev <58301450+410-dev@users.noreply.github.com>
2023-09-06 21:23:21 +0900

    Allows ZLang to have offline installer

--- sys/Library/Developer/bin/install-zlang.sh
+++ sys/Library/Developer/bin/install-zlang.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 if [[ "$(FIRMWARE_INFO "instruction" "zsh")" -ne "1" ]] || [[ "$(FIRMWARE_INFO "instruction" "java")" -ne "1" ]]; then
-    echo "Firmware does not support zlang. (Expected 11, but received $(FIRMWARE_INFO "instruction" "zsh")$(FIRMWARE_INFO "instruction" "java").)"
-    exit 1
+    if [[ "$(regread "SYSTEM/ZLang/BypassCompatibilityCheck")" -eq "1" ]]; then
+        echo "Bypassing compatibility check..."
+    else
+        echo "Firmware does not support zlang. (Expected 11, but received $(FIRMWARE_INFO "instruction" "zsh")$(FIRMWARE_INFO "instruction" "java").)"
+        exit 1
+    fi
 fi
 
-echo "Installing ZLang..."
-curl -Ls https://raw.githubusercontent.com/410-dev/ZLang/main/installer.zsh -o "$CACHE/installer.sh"
-chmod +x "$CACHE/installer.sh"
-mkdir -p "$LIBRARY/Developer/ZLang"
-zsh "$CACHE/installer.sh" "2023MarD-preliminary" "$LIBRARY/Developer/ZLang"
-rm -rf "$CACHE/installer.sh"
+if [[ "$(regread "SYSTEM/ZLang/UseOfflineInstaller")" -eq "1" ]]; then
+    FPATH="$(regread "SYSTEM/ZLang/OfflineInstallerPath")"
+    if [[ ! -f "$FPATH" ]]; then
+        echo "Offline installer not found. Exiting..."
+        exit 1
+    fi
+    echo "Installing ZLang using offline installer..."
+    zsh "$FPATH" "2023MarD-preliminary" "$LIBRARY/Developer/ZLang"
+    exit $? # Exit with the same exit code as the offline installer.
+else
+    echo "Installing ZLang..."
+    curl -Ls https://raw.githubusercontent.com/410-dev/ZLang/main/installer.zsh -o "$CACHE/installer.sh"
+    chmod +x "$CACHE/installer.sh"
+    mkdir -p "$LIBRARY/Developer/ZLang"
+    zsh "$CACHE/installer.sh" "2023MarD-preliminary" "$LIBRARY/Developer/ZLang"
+    rm -rf "$CACHE/installer.sh"
+fi
 
 echo "Installation complete."
